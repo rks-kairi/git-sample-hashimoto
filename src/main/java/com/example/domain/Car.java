f5250485192c373b71178d9fc14b8a82fb2fa60b
hashimotokairi <kairi.hashimoto@rakus.co.jp>
2021-06-04 12:07:31 +0900

    firstcommit

--- src/main/java/com/example/domain/Car.java
+++ src/main/java/com/example/domain/Car.java
@@ -1,5 +1,14 @@
 package com.example.domain;
 
 public class Car {
+	private int speed;
 
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
 }
